4d6e485be98f4269a54407b31fed7e04527c8bc7
GregEyd <37543792+GregEyd@users.noreply.github.com>
2021-08-23 16:54:19 +0100

    Update study_definition.py

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -7,7 +7,36 @@ study = StudyDefinition(
         "rate": "uniform",
         "incidence": 0.5,
     },
-    population=patients.registered_with_one_practice_between(
-        "2019-02-01", "2020-02-01"
-    ),
+
+ population = patients.satisfying(
+     """
+        care_home_type = "PS" OR care_home_type = "PN" OR care_home_type = "PC"
+        """,
+
+care_home_type=patients.care_home_status_as_of(
+    "2020-02-01",
+    categorised_as={
+        "PC":
+        """
+          IsPotentialCareHome
+          AND LocationDoesNotRequireNursing='Y'
+          AND LocationRequiresNursing='N'
+        """,
+        "PN":
+        """
+          IsPotentialCareHome
+          AND LocationDoesNotRequireNursing='N'
+          AND LocationRequiresNursing='Y'
+        """,
+        "PS": "IsPotentialCareHome",
+        "PR": "NOT IsPotentialCareHome",
+        "": "DEFAULT",
+    },
+    return_expectations={
+        "rate": "universal",
+        "category": {"ratios": {"PC": 0.05, "PN": 0.05, "PS": 0.05, "PR": 0.84, "": 0.01},},
+    },
+),
+
+
 )
