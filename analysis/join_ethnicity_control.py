51a6bc2beebc1bb8339a8903d5d169047aa620d9
GregEyd <37543792+GregEyd@users.noreply.github.com>
2021-11-15 16:57:14 +0000

    Updated folder structure for control

---
+++ analysis/join_ethnicity_control.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import os
+
+
+ethnicity_df = pd.read_csv('output/inputs/input_ethnicity.csv')
+
+
+for file in os.listdir('output/control'):
+    if file.startswith('input'):
+        #exclude ethnicity
+        if file.split('_')[1] not in ['ethnicity.csv']:
+            file_path = os.path.join('output/control', file)
+            print(file_path)
+            df = pd.read_csv(file_path)
+            merged_df = df.merge(ethnicity_df, how='left', on='patient_id')
+            
+            merged_df.to_csv(file_path)  
